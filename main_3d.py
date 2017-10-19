a0689e8f3ab977b8bc5bc071c0b65d55566c2de8
sara <saraflore.email@gmail.com>
2017-10-19 17:26:37 +0200

    Modifiche main3d

--- main_3d.py
+++ main_3d.py
@@ -5,8 +5,70 @@ from dataset import *
 from gesture import *
 from test import *
 
+class Joint:
+
+    def __init__(self, x, y, z, nome = None):
+        if (isinstance(x, float)):
+            self.x = x
+        elif (isinstance(y, float)):
+            self.y = y
+        elif (isinstance(z, float)):
+            self.z = z
+        else:
+            print("Errore, non è un float")
+        self.nome = nome
+
+class Frame:
+
+    def __init__(self, num):
+        self.joints = []
+        self.num = num
+
+
+def txt_to_csv():
+    file = open("/home/sara/Scrivania/simple-shrec/gesture_1/gesture_1/finger_1/subject_1/essai_1/skeletons_world.txt", "r").read()
+    f = file.replace('\n', " ").split(" ")
+
+    n_frame = int(len(f) / 66)
+    frames = []
+    index_frame = 0
+
+    print(f[0] + " " + f[1] + " " + f[2])# Frame numero 1
+    print(f[1320] + " " + f[1321] + " " + f[1322]) #Frame numero 20
+    print(f[3300] + " " + f[3301] + " " + f[3302]) #Frame numero 50
+    print(f[5940] + " " + f[5941] + " " + f[5942]) #Frame numero 90
+
+    for index_frame in range(0, n_frame):
+        i = index_frame*66
+        frame = Frame(index_frame)
+
+        for j in range(0, 66, 3):
+            x = float(f[i+j+0])
+            y = float(f[i+j+1])
+            z = float(f[i+j+2])
+            joint = Joint(x,y,z)
+            frame.joints.append(joint)
+
+        frames.append(frame)
+
+    return frames
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Indica quale tipo di debug voglio avviare (0 = debug GestureModel, 1 = debug DeclarativeModel, 2 = debug Dataset Shrek in 2 Dimensioni)
-debug_mode = 2
+debug_mode = 3
 
 #### Debug GestureModel (Line3D e Point3D) ####
 if debug_mode == 0:
@@ -52,3 +114,10 @@ if debug_mode == 2:
         plt.plot(result[:, 0], result[:, 1])
         plt.title(gesture)
         plt.show()
+
+
+if debug_mode == 3:
+ txt_to_csv()
+
+
+
